1a038df06981095c1b0c8e8052d897821d9d38cd
SPors0506 <53863350+SPors0506@users.noreply.github.com>
2022-09-22 15:32:40 +0200

    Added SBN Camera Listener

---
+++ SBNCamListener/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using NATS.Client;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Listening in on SBN Camera...");
+
+var cfg = new Dictionary<string, string>() {
+    {"host", "video-beta.sbncloud.com"},
+    {"port", "4222"},
+    {"token", "secret"},
+    {"chan", "camera.>"},
+    {"excl", "camera.hiknet.>"}
+};
+
+long msgNo = 0;
+ConnectionFactory cf = new ConnectionFactory();
+Options opts = ConnectionFactory.GetDefaultOptions();
+opts.Url = String.Format("nats://{0}@{1}:{2}", cfg["token"], cfg["host"], cfg["port"]);
+
+using (IConnection c = cf.CreateConnection(opts))
+{
+
+    IAsyncSubscription s_id = c.SubscribeAsync("camera.>",
+        (sender, args) =>
+        {
+            if (args.Message.Subject.StartsWith("camera.hiknet."))
+                return;
+            var no = Interlocked.Increment(ref msgNo);
+            var str = Encoding.UTF8.GetString(args.Message.Data);
+            Console.WriteLine($"[#{no}] {str}");
+        });
+
+    Console.ReadLine();
+}
+Console.WriteLine("DONE on SBN Camera...");
